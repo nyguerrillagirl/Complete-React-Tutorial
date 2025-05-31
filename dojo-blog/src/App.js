4f7945aa3aaa54e1869778f5adea5ecb70da3321
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-05-31 16:36:59 -0400

    my_lesson_06

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
-
+import './index.css';
 // This is the root component of the React application.
 function App() {
 
--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,10 +1,10 @@
 const  Navbar = () => {
     return (  
-        <nav className="">
+        <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" >New Blog</a>
            </div>
         </nav>
     );
