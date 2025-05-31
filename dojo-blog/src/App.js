95ae105c79c9961e1aa229561e4bd7a0e0796e09
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-05-31 15:27:08 -0400

    lesson04

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 
 // This is the root component of the React application.
 function App() {
+  const title = "Welcome to the new Blog";
+  const likes = 50;
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times!</p>
+        <p>{ 10 }</p>
+        <p>{ 'hello, ninjas'} </p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link }>Google Site</a>
       </div>
     </div>
   );
