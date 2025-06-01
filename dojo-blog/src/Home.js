603b98f4f0d08190369671d45699375917f84cd6
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-01 10:13:44 -0400

    my-lesson-13

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,6 +2,11 @@ import { useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+    
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -9,9 +14,8 @@ const Home = () => {
     ]);
     return (  
         <div className="home">
-            <BlogList  blogs={blogs} title="All Blogs!"/>
-            <BlogList  blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blogs!"/>
-         </div>
+            <BlogList  blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+          </div>
     );
 }
  
