2eb09b756744586a792c63839ffde447dfd0d412
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-05 11:48:26 -0400

    my_lesson_19

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
-
+    const [error, setError] = useState(null);
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
@@ -19,16 +19,19 @@ const Home = () => {
                 .then(data => {
                     setBlogs(data);
                     setIsPending(false);
+                    setError(null);
                 })
                 .catch(err => {
-                    console.log(err.message);
+                    setError(err.message);
+                    setIsPending(false);
                 });
-            }, 3000); 
+            }, 1000); 
         
     }, []);
 
     return (  
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList  blogs={blogs} title="All Blogs!"/>}
 
