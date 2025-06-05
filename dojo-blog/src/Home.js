2eb25ea79b35fa1697636ce75d986db6586001b6
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-05 11:27:25 -0400

    my_lesson_18

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -5,29 +5,31 @@ const Home = () => {
 
     
     const [blogs, setBlogs] = useState(null);
-
-
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if (!res.ok) {
-                    throw Error('Could not fetch the data for that resource');
-                }
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                setBlogs(data);
-            })
-            .catch(err => {
-                console.log(err.message);
-            });
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('Could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                });
+            }, 3000); 
         
     }, []);
 
     return (  
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList  blogs={blogs} title="All Blogs!"/>}
 
           </div>
