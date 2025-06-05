5b93218a2d8fe891d4b6eab830ed731f2148cfd1
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-05 10:11:51 -0400

    my_lesson_14

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -12,6 +12,12 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+    useEffect(() => {
+        console.log('useEffect ran');
+        console.log(blogs);
+    });
+
     return (  
         <div className="home">
             <BlogList  blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
