a3d4e2db79490529eb9a9c3758172f34bb1bdbed
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-15 20:02:01 -0400

    my-lesson-24

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -7,8 +7,11 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, {  signal: abortCont.signal })
                 .then(res => {
                     if (!res.ok) {
                         throw Error('Could not fetch the data for that resource');
@@ -21,10 +24,17 @@ const useFetch = (url) => {
                     setError(null);
                 })
                 .catch(err => {
-                    setError(err.message);
-                    setIsPending(false);
+                    if (err.name === 'AbortError') {
+                        console.log('Fetch aborted');
+                    } else {
+                        setError(err.message);
+                        setIsPending(false);
+                    }
+
                 });
             }, 1000); 
+
+        return () => abortCont.abort();
     
     }, [url]);
 
