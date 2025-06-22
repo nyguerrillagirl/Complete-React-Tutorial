700745c11c519fd0e16f71cb52a18eb70b76b162
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-22 08:19:26 -0400

    my-lesson-27

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text" 
+                    required placeholder="Blog title"
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)}/>
+                 <label>Blog body:</label>
+                <textarea
+                    required 
+                    rows="10"
+                    placeholder="Write your blog here..."
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}>
+                </textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                    <option value="luigi">Luigi</option>
+                </select>
+                <button>Add Blog</button>
+                
+            </form>
         </div>
     );
 
